59d20568e041086e6ad030b8652bbe2eabdc03f4
Meggers and Bradley!!! <mb2017@Meggerss-MacBook-Pro.local>
2021-11-09 22:44:58 -0600

    home page list wireframe res bug

--- routers/people.js
+++ routers/people.js
@@ -11,23 +11,3 @@ router.get('/', function(req, res) {
 });
 
 module.exports = router; 
-
-/*
-// request the data from third party API
-	request(
-    { 
-    	method: 'GET',
-    	uri: 'http://mysafeinfo.com/api/data?list=englishmonarchs&format=json'
-   	},
-  	function (error, resp, body) {
-  		// if the response is an error, display the error page
-      if (error) {
-      	res.render('pages/error', { error: err });
-    	}
-    	// otherwise render the home page with the fetched data
-    	else {
-    		res.render('pages/home', { kings: JSON.parse(body) });
-    	}
-    }
-  );
-*/
