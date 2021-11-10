de4af2e286ee656ac5777a9d5b2de110f3b3754b
Meggers and Bradley!!! <mb2017@Meggerss-MacBook-Pro.local>
2021-11-09 21:24:32 -0600

    removing 404 and 500 from index

--- index.js
+++ index.js
@@ -17,18 +17,6 @@ app.set('view engine', 'handlebars');
 
 app.use('/',peopleRouter);
 
-// make a 404 error page
-app.use(function (req, res) {
-	res.status(404);
-	res.render('pages/404');
-});
-
-// handle other errors
-app.use(function (err, req, res) {
-  res.status(500);
-  res.render('pages/error', { error: err });
-});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
