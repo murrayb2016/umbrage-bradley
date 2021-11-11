8af699a37ea379f55cabafceb8ec12139ece66b4
Meggers and Bradley!!! <mb2017@Meggerss-MacBook-Pro.local>
2021-11-10 21:30:38 -0600

    login template

--- routers/people.js
+++ routers/people.js
@@ -1,12 +1,24 @@
 // require Express application framework
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 // require 'request' module that allows to make external HTTP requests
-var request = require('request');
+const request = require('request');
+const bodyParser = require("body-parser");
 
 // define the app routes
+
+//Homepage for list of people
 router.get('/', function(req, res) {
+	res.render('pages/home');
+});
+
+router.get('/login', function(req, res) {
+	res.render('pages/login');
+});
+
+router.post('/login', function(req, res) {
+	console.log(req.body)
 	res.render('pages/home');
 });
 
