98e47d6c6ff19a8fc0a1747d00a5c610695f7704
Meggers and Bradley!!! <mb2017@Meggerss-MacBook-Pro.local>
2021-11-11 16:43:11 -0600

    login async

--- routers/people.js
+++ routers/people.js
@@ -15,7 +15,7 @@ router.get('/login', function(req, res) {
 	res.render('pages/login');
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
 	console.log(req.body)
 	res.render('pages/home');
 });
