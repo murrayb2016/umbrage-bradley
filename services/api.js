412887a4a72e5737361bb5d7523569603532a3b5
Meggers and Bradley!!! <mb2017@Meggerss-MacBook-Pro.local>
2021-11-11 17:09:47 -0600

    enable es6

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 // require 'request' module that allows to make external HTTP requests
 const request =  require('superagent');
 
-async function login(){
+function login(){
     return request.get('https://umbrage-interview-api.herokuapp.com/login').set({username: req.body.email, password: req.body.password});
 }
 
