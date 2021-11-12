b32c3f1fdc8d55692c636876bff0329ca6f75485
Meggers and Bradley!!! <mb2017@Meggerss-MacBook-Pro.local>
2021-11-11 19:09:40 -0600

    retrieving token

--- routers/people.js
+++ routers/people.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const superagent = require('superagent');
 
+const ls = require('local-storage');
+
 // define the app routes
 
 //Homepage for list of people
@@ -24,6 +26,7 @@ router.post('/login', (req, res) => {
 	.end((err, res) => {
 	  // Calling the end function will send the request
 	  console.log(JSON.stringify(res))
+	  console.log(res.text)
 	});
 	   res.render('pages/home');
 });
