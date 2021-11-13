1412a899c1f4fcec73efb239efa33153eafb962d
Meggers and Bradley!!! <mb2017@Meggerss-MacBook-Pro.local>
2021-11-12 21:19:52 -0600

    adding cookies for auth

--- routers/people.js
+++ routers/people.js
@@ -4,9 +4,6 @@ const router = express.Router();
 
 const superagent = require('superagent');
 
-require('dotenv').config();
-
-
 // define the app routes
 
 //Homepage for list of people
@@ -19,21 +16,5 @@ router.get('/login', function(req, res) {
 	res.render('pages/login');
 });
 
-router.post('/login', (req, res) => {
-	console.log(req.body)
-	superagent
-	.post('https://umbrage-interview-api.herokuapp.com/login')
-	.send({ username: req.body.email, password: req.body.password }) // sends a JSON post body
-	.set('accept', 'json')
-	.end((err, res) => { 
-	  // Calling the end function will send the request
-	  let resp = res.text;
-	  let token = JSON.parse(resp).access_token;
-	  var date = new Date();
-	  res.cookie('token', token, {expire: date.getDate() + 1,  httpOnly: false}); 
-	  console.log(err)
-	});
-	   res.render('pages/home');  
-});
 
 module.exports = router; 
