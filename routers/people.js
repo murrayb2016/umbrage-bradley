a184195c4e2c207af57fe8a532efe1a7af1f8d16
Meggers and Bradley!!! <mb2017@Meggerss-MacBook-Pro.local>
2021-11-12 20:27:04 -0600

    adding local storage

--- routers/people.js
+++ routers/people.js
@@ -4,13 +4,17 @@ const router = express.Router();
 
 const superagent = require('superagent');
 
-require('dotenv').config()
+require('dotenv').config();
 
+if (typeof localStorage === "undefined" || localStorage === null) {
+	var LocalStorage = require('node-localstorage').LocalStorage;
+	localStorage = new LocalStorage('./scratch');
+}
 // define the app routes
 
 //Homepage for list of people
 router.get('/', function(req, res) {
-	console.log(process.env.token)
+	console.log(localStorage.getItem('token'));
 	res.render('pages/home');
 });
 
@@ -28,7 +32,7 @@ router.post('/login', (req, res) => {
 	  // Calling the end function will send the request
 	  let resp = res.text;
 	  let token = JSON.parse(resp).access_token;
-	  process.env.token = token; 
+	  localStorage.setItem('token', token);
 	  console.log(err)
 	});
 	   res.render('pages/home'); 
