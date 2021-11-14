4aac0c7ebcfcd9fb89a75647c12980a3ed3818e5
Meggers and Bradley!!! <mb2017@Meggerss-MacBook-Pro.local>
2021-11-13 21:14:14 -0600

    adding session and cookie for auth

--- routers/people.js
+++ routers/people.js
@@ -24,7 +24,7 @@ router.get('/login', function(req, res) {
 router.post('/login', (req, res) => {
 	// console.log(req.body)
 	session=req.session;
-	console.log(req.body)
+	console.log(req)
 	superagent
 	.post('https://umbrage-interview-api.herokuapp.com/login')
 	.send({ username: req.body.email, password: req.body.password }) // sends a JSON post body
