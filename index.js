fe346dbd183a53d6554beefd384914b36700b074
Meggers and Bradley!!! <mb2017@Meggerss-MacBook-Pro.local>
2021-11-13 20:02:13 -0600

    adding session and cookie for auth

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 const oneDay = 1000 * 60 * 60 * 24;
 app.use(sessions({
-    secret: process.env.SECRET,
+    secret: 'test',
     saveUninitialized:true,
     cookie: { maxAge: oneDay },
     resave: false 
