29b820830bd3d21f89bcda9b9146518b3463f187
Meggers and Bradley!!! <mb2017@Meggerss-MacBook-Pro.local>
2021-11-14 00:28:33 -0600

    get peoples

--- routers/people.js
+++ routers/people.js
@@ -13,7 +13,6 @@ router.get('/', function(req, res) {
 	if(token){
 		superagent
 	.get('https://umbrage-interview-api.herokuapp.com/people')
-	.set('accept', 'json')
 	.auth('Authorization',token, { type: 'bearer' })
 	.end((err, res) => { 
 	  console.log(res)
